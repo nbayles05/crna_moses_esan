18fd0b855cd3284b42bca4334bff6ebeb7232d85
nbayles <nbayles@crestfinancial.com>
2018-01-29 16:26:28 -0700

    fix install-dependencies.bat file

--- install-dependencies.bat
+++ install-dependencies.bat
@@ -1,9 +1,1 @@
-yarn add redux
-yarn add react-redux 
-yarn add redux-thunk 
-yarn add react-native-elements
-yarn add react-native-router-flux
-yarn add axios
-yarn add react-native-redux-listener
-yarn add firebase
-yarn add react-native-loading-spinner-overlay
+yarn add redux react-redux redux-thunk react-native-elements react-native-router-flux axios react-native-redux-listener firebase react-native-loading-spinner-overlay
