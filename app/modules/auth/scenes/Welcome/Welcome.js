da5e96498630bf20ca45116d965ef1940607ec1d
nbayles <nbayles@crestfinancial.com>
2018-01-30 09:57:44 -0700

    step 9

--- app/modules/auth/scenes/Welcome/Welcome.js
+++ app/modules/auth/scenes/Welcome/Welcome.js
@@ -1,62 +1,83 @@
 import React from 'react';
-import {Text, View, Button as RNButton} from 'react-native';
+import {Text, View} from 'react-native';
 
 import {Button, SocialIcon} from 'react-native-elements'
 import {Actions} from 'react-native-router-flux'
 import {connect} from 'react-redux';
+import {Facebook} from 'expo';
 
-import {actions as auth, constants as c} from "../../index"
-const {} = auth;
+import {actions as auth, constants as c} from "../"
+const {signInWithFacebook} = auth;
 
-import styles from "./styles"
-import AuthContainer from "../../components/AuthContainer"
-
-import { theme } from "../../index"
-const { color } = theme;
+import styles from "../styles"
+import AuthContainer from "../components/AuthContainer"
 
 class Welcome extends React.Component {
     constructor() {
         super();
         this.state = {}
 
-        //this.onSuccess = this.onSuccess.bind(this);
-        //this.onError = this.onError.bind(this);
+        this.onSuccess = this.onSuccess.bind(this);
+        this.onError = this.onError.bind(this);
+        this.onSignInWithFacebook = this.onSignInWithFacebook.bind(this);
     }
-    
+
+    //get users permission authorization (ret: facebook token)
+    async onSignInWithFacebook() {
+        const options = {permissions: ['public_profile', 'email'],}
+        const {type, token} = await Facebook.logInWithReadPermissionsAsync(c.FACEBOOK_APP_ID, options);
+
+        if (type === 'success') {
+            this.props.signInWithFacebook(token, this.onSuccess, this.onError)
+        }
+    }
+
+    onSuccess() {
+        Actions.Main()
+    }
+
+    onError(error) {
+        alert(error.message);
+    }
+
     render() {
         return (
             <AuthContainer>
-                <View style={styles.container}>
+                <View style={styles.wrapper}>
+                    <Text style={styles.appTitle}>APP Name</Text>
+                </View>
+                <View>
                     <SocialIcon
                         raised
                         button
                         type='facebook'
-                        title='Continue with Facebook'
+                        title='Sign in with Facebook'
                         iconSize={19}
-                        style={[styles.buttonContainer, styles.socialButton]}
+                        style={styles.fbButton}
                         onPress={this.onSignInWithFacebook}/>
-                </View>
-                <View style={styles.container}>
-                    <Button
-                        raised
-                        title={'Sign Up with Email'}
-                        borderRadius={4}
-                        containerViewStyle={styles.buttonContainer}
-                        buttonStyle={styles.button}
-                        textStyle={styles.buttonText}
-                        onPress={Actions.Register}/>
-                    <RNButton
-                        title={'Already have an account?'}
-                        color={color.main}
-                        onPress={Actions.Login}/>
 
-                    <Text style={styles.termsAndCondition}>
-                        By signing up, you agree with the Terms of Service and Privacy Policy</Text>
+                    <View style={styles.bottomContainer}>
+                        <Button
+                            raised
+                            title={'Sign up'}
+                            borderRadius={4}
+                            containerViewStyle={styles.buttonContainer}
+                            buttonStyle={{}}
+                            textStyle={styles.buttonText}
+                            onPress={Actions.Register}/>
+                        <Button
+                            raised
+                            title={'Login'}
+                            borderRadius={4}
+                            containerViewStyle={[styles.buttonContainer]}
+                            buttonStyle={{}}
+                            textStyle={styles.buttonText}
+                            onPress={Actions.Login}/>
+                    </View>
                 </View>
             </AuthContainer>
         );
     }
 }
 
-
-export default connect(null, {})(Welcome);
+export default connect(null, {signInWithFacebook})(Welcome);
